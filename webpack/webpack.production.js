896546425349931d1d61bfc4e5aab9e3194a7e68
Joshua Obasaju <obasajujoshua31@gmail.com>
2019-03-05 20:03:09 +0100

    chore(setup): setup empty react
    - add webpack config for both production and development environment
    - add heroku postbuild script and Procfile to host app at heroku
    - add babel to transpile at runtime
    
    [Delivers #163426571]

---
+++ webpack/webpack.production.js
@@ -0,0 +1,37 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const uglifyJS = new UglifyJsPlugin({
+  exclude: /node_modules/,
+  cache: true,
+  parallel: true
+});
+
+module.exports = {
+  devtool: "source-map",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  optimization: {
+    minimizer: [uglifyJS],
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.scss$/,
+          chunks: "all",
+          enforce: true
+        }
+      }
+    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[hash].css",
+      chunkFilename: "css/[id].[hash].css"
+    }),
+    uglifyJS
+  ]
+};
