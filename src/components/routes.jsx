6d5cb1cfe0b263df44d09adc11618e11dcc581fa
Kingsley Frank-Demesi <kingsleyfrankdemesi@gmail.com>
2019-03-26 12:38:46 +0100

    articles

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,7 +1,8 @@
 import HomePage from "<pages>/Homepage";
 import SignInWrapper from "<auth>/SignIn";
 import SignUpWrapper from "<auth>/SignUp";
-// import ProfilePage from "<pages>/Profile";
+import ProfilePage from "<pages>/Profile";
+import ViewArticlePage from "<pages>/viewArticle";
 import SocialRedirect from "<auth>/SocialAuth";
 import PasswordRequest from "<auth>/RequestPasswordReset";
 import ResetPassword from "<auth>/ResetPassword";
@@ -48,6 +49,11 @@ const Routes = {
       component: VerifyAccount
     }
   ],
-  secured: []
+  secured: [
+    {
+      path: "/articles/view",
+      component: ViewArticlePage
+    }
+  ]
 };
 export default Routes;
